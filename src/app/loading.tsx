840b9b5484399e98df42a9ab1da111c10836892f
Alisher Issayev <aliisherka@mail.ru>
2025-05-23 14:00:47 +0200

    Initial commit

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <main className='flex items-center justify-center min-h-screen'>
+      <p className='text-lg text-gray-600 animate-pulse'>Loading...</p>
+    </main>
+  );
+}
---
+++ src/app/places/[id]/page.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link"
+import Image from 'next/image';
+
+export default async function PlacePage({ params }) {
+  const res = await fetch(`https://airbnb-backend-qwox.onrender.com/houses/${params.id}`, {
+    next: { revalidate: 6000 },
+  })
+  if (!res.ok) {
+    return <h1>House not found</h1>
+  }
+
+  const place = await res.json()
+
+  return (
+    <main className='p-6 max-w-4xl mx-auto'>
+      <Link href='/' className='text-blue-600 mb-4 inline-block'>
+        ← Back
+      </Link>
+
+      <h1 className='text-3xl font-bold mb-4'>{place.title}</h1>
+
+      <Image
+        src={place.images[0]}
+        alt={place.title}
+        width={600}
+        height={400}
+        className='rounded-lg object-cover mb-6'
+      />
+
+      <p className='mb-2 font-semibold text-lg'>Price: {place.price}$ night</p>
+      <p className='mb-4'>{place.description}</p>
+
+    </main>
+  )
+}
